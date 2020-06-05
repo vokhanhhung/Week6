42171a01b7a77bd393cb451d29718ee0c3046fe9
vokhanhhung <vokhanhhung1998@gmail.com>
2020-06-05 22:03:47 +0700

    Add retrofit

--- app/src/main/java/com/example/week6/activity/MainActivity.kt
+++ app/src/main/java/com/example/week6/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.week6
+package com.example.week6.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.week6.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
