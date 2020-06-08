57e33488a39b8f762a813db87e2d1b467600ed09
vokhanhhung <vokhanhhung1998@gmail.com>
2020-06-08 19:04:46 +0700

    Complete

--- app/src/main/java/com/example/week6/activity/MainActivity.kt
+++ app/src/main/java/com/example/week6/activity/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.example.week6.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.week6.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Navigation.findNavController(
+            this,
+            R.id.nav_host_fragment
+        ).let {
+            NavigationUI.setupWithNavController(bttm_nav, it)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_option, menu)
+        return true
     }
 }
